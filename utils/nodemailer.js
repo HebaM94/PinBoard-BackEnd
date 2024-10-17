4ca2af17ff1e3764f570872a419b9d2190222060
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-10-17 20:12:32 +0300

    add mailtrap data

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,23 +1,23 @@
 import nodemailer from 'nodemailer';
 
-export async function sendEmail(mailOptions) {
-    try {
+export default async function sendEmail(mailOptions) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'sandbox.smtp.mailtrap.io', // change after deployment to the actual domain in  mailtrap
+      port: 465, // Or 465 for SSL
+      secure: false, // true for SSL
+      auth: {
+        user: '650fe01f15a206',
+        pass: 'efceab0c3c45fd',
+      },
+    });
 
-      const transporter = nodemailer.createTransport({
-        host: 'live.smtp.mailtrap.io', // change after deployment to the actual domain in  mailtrap
-        port: 587, // Or 465 for SSL
-        secure: false, // true for SSL
-        auth: {
-          user: 'smtp@mailtrap.io',
-          pass: 'd5d364ace96f2b1b1eb97a13addd7825',
-        },
-      });
-      
-      await transporter.sendMail(mailOptions);
-      console.log(`Email sent to ${mailOptions.to}`);
-      return response.status(200).json({ message: 'Please check your email' });
-    } catch (err) {
-        console.error('Failed to send email:', err);
-        throw err;
-    }
+    const result = await transporter.sendMail(mailOptions);
+    console.log(result);
+    console.log(`Email sent to ${mailOptions.to}`);
+    // return .status(200).json({ message: 'Please check your email' });
+  } catch (err) {
+    console.error('Failed to send email:', err);
+    throw err;
   }
+}
